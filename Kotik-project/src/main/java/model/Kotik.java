52146a1c46ed1decb2496c403d635108e3aede9a
Alex <Alrast95@gmail.com>
2021-05-12 21:53:57 +0400

    исправленная версия

--- Kotik-project/src/main/java/model/Kotik.java
+++ Kotik-project/src/main/java/model/Kotik.java
@@ -3,6 +3,7 @@ package model;
 import java.util.Random;
 
 public class Kotik {
+
     final Random random = new Random();
     private String name;
     private double weight;
@@ -10,35 +11,10 @@ public class Kotik {
     private int prettiness;
     private int distance;
     private int levelOfHungry;
-    static long kotikCount;
-
-    public String getName() {
-        return name;
-    }
-    public double getWeight() {
-        return weight;
-    }
-    public String getMeow() {
-        return meow;
-    }
-    public int getPrettiness() {
-        return prettiness;
-    }
-    public int getDistance() {
-        return distance;
-    }
+    private static long kotikCount;
 
     public Kotik() {
-kotikCount++;
-    }
-
-    public  void setKotik(int prettiness, double weight, String name, String meow, int levelOfHungry, int distance) {
-        this.prettiness = prettiness;
-        this.weight = weight;
-        this.name = name;
-        this.meow = meow;
-        this.levelOfHungry=levelOfHungry;
-        this.distance = distance;
+        kotikCount++;
     }
 
     public Kotik(String name, double weight, String meow, int prettiness, int levelOfHungry, int distance) {
@@ -46,66 +22,99 @@ kotikCount++;
         this.weight = weight;
         this.meow = meow;
         this.prettiness = prettiness;
-       this.distance = distance;
+        this.distance = distance;
         this.levelOfHungry = levelOfHungry;
         kotikCount++;
     }
 
+    public static long getKotikCount() {
+        return kotikCount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public String getMeow() {
+        return meow;
+    }
+
+    public int getPrettiness() {
+        return prettiness;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
 
 
-    public boolean run(){
+    public void setKotik(int prettiness, double weight, String name, String meow, int levelOfHungry, int distance) {
+        this.prettiness = prettiness;
+        this.weight = weight;
+        this.name = name;
+        this.meow = meow;
+        this.levelOfHungry = levelOfHungry;
+        this.distance = distance;
+    }
 
-        if (levelOfHungry>0) {
+
+    public boolean run() {
+
+        if (levelOfHungry > 0) {
             levelOfHungry--;
             distance++;
             System.out.println("котик побегал");
             return true;
-        }
-        else {
-            eat();
-        return false;
+        } else {
+            System.out.println("котик голоден");
+            return false;
         }
     }
-    public boolean play(){
 
-        if (levelOfHungry>0) {
+    public boolean play() {
+
+        if (levelOfHungry > 0) {
             levelOfHungry--;
             prettiness++;
             System.out.println("котик поиграл");
             return true;
-        }
-        else {
-            eat();
+        } else {
+            System.out.println("котик голоден");
             return false;
         }
     }
-    public boolean sleep(){
 
-        if (levelOfHungry>0) {
+    public boolean sleep() {
+
+        if (levelOfHungry > 0) {
             levelOfHungry--;
             System.out.println("котик поспал");
             return true;
-        }
-        else {
-            eat();
+        } else {
+            System.out.println("котик голоден");
             return false;
         }
     }
-    public boolean chaseMouse(){
-        if (levelOfHungry>0) {
+
+    public boolean chaseMouse() {
+        if (levelOfHungry > 0) {
             levelOfHungry--;
-           System.out.println("котик догнал мышь");
+            System.out.println("котик догнал мышь");
             return true;
-        }
-        else {
-            eat();
+        } else {
+            System.out.println("котик голоден");
             return false;
         }
     }
-    public boolean meow(){
-        if (levelOfHungry>0) {
+
+    public boolean meow() {
+        if (levelOfHungry > 0) {
             levelOfHungry--;
-            switch (random.nextInt(3)){
+            switch (random.nextInt(3)) {
                 case 0:
                     System.out.println(meow.concat("meow"));
                     break;
@@ -116,43 +125,34 @@ kotikCount++;
                     System.out.println(meow.concat("meeeoow"));
                     break;
 
-        }
-            System.out.println("котик помурчал");
-        return true;
-        }
-        else {
-            eat();
+            }
+            return true;
+        } else {
+            System.out.println("котик голоден");
             return false;
         }
     }
+
     public int eat(int foodIncome) {
-        return levelOfHungry+=foodIncome;
+        return levelOfHungry += foodIncome;
     }
+
     public int eat(int foodIncome, String food) {
-        return levelOfHungry+=foodIncome;
+        return levelOfHungry += foodIncome;
     }
+
     public void eat() {
-eat(12,"fish");
+        eat(12, "fish");
     }
 
     public void liveAnotherDay() {
-        for (int i = 0; i <24 ; i++) {
-            switch (random.nextInt(4)){
-                case 0:
-                    System.out.println( play());
-                    break;
-                case 1:
-                    System.out.println(chaseMouse());
-                    break;
-                case 2:
-                    System.out.println(sleep());
-                    break;
-                case 3:
-                    System.out.println(meow());
-                    break;
-                case 4:
-                    System.out.println(run());
-                    break;
+        for (int i = 0; i < 24; i++) {
+            switch (random.nextInt(4)) {
+                case 0 -> play();
+                case 1 -> chaseMouse();
+                case 2 -> sleep();
+                case 3 -> meow();
+                case 4 -> run();
             }
         }
 
